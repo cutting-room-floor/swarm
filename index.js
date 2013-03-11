a9fceaa468a18dcd74d738759c8ee3f15ce9e495
Will White <will@mapbox.com>
2013-03-11 19:15:23 -0400

    Remove puppet-specific code.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var Step = require('step');
 var _ = require('underscore');
 var fs = require('fs');
-var yamlish = require('yamlish');
 var instanceMetadata = require('./lib/instance-metadata');
 var ec2Api= require('./lib/ec2-api');
 
@@ -12,11 +11,10 @@ var optimist = require('optimist')
            'Usage: $0 <command> [options]\n\n' +
            'Available commands:\n' +
            '  list: list active swarms\n' +
-           '  metadata: load given attribute from EC2 API\n' +
-           '  classify: return list of puppet classes in YAML format suitable for use with puppet\'s ENC feature')
+           '  metadata: load given attribute from EC2 API\n')
     .describe('config', 'Path to JSON configuration file that contains awsKey and awsSecret.')
     .describe('attribute', 'The EC2 API instance attribute to load from the swarm. Required for the metadata command.')
-    .describe('filter', 'Applies a filter to results based on EC2 instance attributes and tags. Use `filter.<attributeName>`. Multiple filters are applied with the AND operator. Required for the classify command and optional for the metadata command.')
+    .describe('filter', 'Applies a filter to results based on EC2 instance attributes and tags. Use `filter.<attributeName>`. Multiple filters are applied with the AND operator. Optional for the metadata command.')
     .describe('awsKey', 'awsKey, overrides the value in gconfig file if both are provided.')
     .describe('awsSecret', 'awsSecret, overrides the value in config file if both are provided.')
     .default('regions', 'us-east-1,us-west-1,us-west-2,eu-west-1,ap-southeast-1,ap-northeast-1,sa-east-1');
@@ -29,15 +27,9 @@ if (argv.help) {
 
 var command = argv._[0];
 if (!command) command = 'list';
-if (!_(['list', 'metadata', 'classify']).include(command)) {
+if (!_(['list', 'metadata']).include(command)) {
     optimist.showHelp();
     console.error('Invalid command %s.', command);
-    process.exit(1);
-}
-
-if (command === 'classify' && !argv.filter) {
-    optimist.showHelp();
-    console.error('Missing --filter option required for %s command.', command);
     process.exit(1);
 }
 
@@ -107,28 +99,6 @@ swarm.metadata = function() {
     });
 
 }
-
-// ENC Interface for Puppet and EC2 Tags
-//
-// Accepts an EC2 private hostname as an argument. Looks up the tags
-// on the EC2 instance and outputs puppet classes as YAML.
-//
-// See puppet docs on ENC:
-// http://docs.puppetlabs.com/guides/external_nodes.html
-//
-swarm.classify = function() {
-    Step(function() {
-        ec2Api.loadInstances(ec2Api.createClients(config, regions), argv.filter, this);
-    }, function(err, instances) {
-        if (err) throw err;
-        var instance = _(instances).first();
-        var hash = {};
-        if (instance['PuppetClasses']) hash['classes'] = JSON.parse(instance['PuppetClasses']);
-        if (instance['PuppetParameters']) hash['parameters'] = JSON.parse(instance['PuppetParameters']);
-        if (instance['PuppetEnvironment']) hash['environment'] = instance['PuppetEnvironment'];
-        console.log(yamlish.encode(hash));
-    });
-};
 
 // If any supported arguments have the `_self` value we resolve then first, and
 // then actually run the command.
